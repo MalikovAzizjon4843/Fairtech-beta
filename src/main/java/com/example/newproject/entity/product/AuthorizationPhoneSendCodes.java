108d24b77532c6532d70347ccc459b81ae0612ba
Malikov Azizjon <malikovaziz20034843@gmail.com>
2023-07-20 16:40:28 +0500

    entity has done

--- src/main/java/com/example/newproject/entity/product/AuthorizationPhoneSendCodes.java
+++ src/main/java/com/example/newproject/entity/product/AuthorizationPhoneSendCodes.java
@@ -1,8 +1,38 @@
 package com.example.newproject.entity.product;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.example.newproject.entity.Auditable;
+import java.util.Date;
+
 /**
  * @author Malikov Azizjon    Fairtech-Beta       20.07.2023       16:19
  */
 
-public class AuthorizationPhoneSendCodes {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "product_users_sended_codes")
+public class AuthorizationPhoneSendCodes extends Auditable<String> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PRODUCT_USERS_SEQUENCE")
+    @SequenceGenerator(sequenceName = "PRODUCT_USERS_SEQUENCE", allocationSize = 1, name = "PRODUCT_USERS_SEQUENCE")
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "sended_code")
+    private String code;
+
+    @Column(name = "sended_date")
+    private Date sendedDate;
+
+
+
 }
