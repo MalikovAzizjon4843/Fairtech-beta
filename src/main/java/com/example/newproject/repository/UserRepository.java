4299f434098af6633c53d1c499f8fecc85b9e7dc
Malikov Azizjon <malikovaziz20034843@gmail.com>
2023-07-20 14:27:44 +0500

    edited query

--- src/main/java/com/example/newproject/repository/UserRepository.java
+++ src/main/java/com/example/newproject/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.newproject.repository;
 import com.example.newproject.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -19,9 +18,9 @@ public interface UserRepository extends CrudRepository<User, Long> {
 
 //    List<User> findAllByNameRuLikeOrNameUzLikeOrNameLtLikeOrNameEnLike(String nameRu, String nameUz, String nameLt, String nameEn);
 
-    @Query("select m from User m order by m.name")
-    Page<User> findAll(Pageable paging);
+    @Query("select m from User m order by m.nameEn ")
+    Page<User> findAll1(Pageable paging);
 
-    @Query("select m from User m where (lower(m.name) like %?1% or lower(m.mxikCode) like %?1%)")
+    @Query("select m from User m where (lower(m.nameEn) like %?1% or lower(m.nameUz) like %?1%)")
     Page<User> findAll(String keyword, Pageable paging);
 }
